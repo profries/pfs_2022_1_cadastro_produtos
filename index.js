72c5fee3c14c4d48b18c193d1ff1828b2b29c5f2
Luis Henrique Ries <prof.ries@gmail.com>
2022-03-23 21:40:30 -0300

    Criacao das rotas das APIs de cadastro

--- index.js
+++ index.js
@@ -2,16 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+let listaProdutos = [
+    {id:1, nome:"Pastel", preco:7}
+];
+let idAutoIncrement = 2;
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.get('/api/produtos', (req, res) => {
+    res.json(listaProdutos);
 })
 
-app.get('/hello', (req, res) => {
-    if(req.query && req.query.nome) { 
-        res.send('<h1>Hello' + req.query.nome +'!</h1>')
+app.get('/api/produtos/:id', (req, res) => {
+    const id = req.params.id;
+
+    const produto = listaProdutos.find(
+        (produto) => produto.id == id
+    );
+
+    if(!produto){
+        res.status(404).json({erro:"Produto não encontrado!"});        
     }
     else {
-        res.send('<h1>Hello Anonymous!</h1>');
+        res.json(produto);
+    }
+})
+
+app.post("/api/produtos", (req, res) => {
+    const produto = req.body;
+    if(produto && produto.nome && produto.preco){
+        produto.id = idAutoIncrement++;
+        listaProdutos.push(produto);
+        res.status(201).json(produto);
+    }
+    else {
+        res.status(400).json({erro:"Falta parametros de produto"})
     }
 })
 
